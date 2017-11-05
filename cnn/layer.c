ce05fe28a4c6c5913244479595a8fb46008bb8b9
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-11-05 19:29:13 +0100

    implement layer

---
+++ cnn/layer.c
@@ -0,0 +1,40 @@
+#include "layer.h"
+#include "convLayer.h"
+#include "fcLayer.h"
+#include "reluLayer.h"
+#include "poolLayer.h"
+
+void activateLayer(Layer* layer, Tensor* in){
+    switch(layer->type){
+        case conv:
+            activateConvLayer((ConvLayer *) layer, in);
+            break;
+        case fc:
+            activateFcLayer((FcLayer *) layer, in);
+            break;
+        case relu:
+            activateReluLayer((ReluLayer *) layer, in);
+            break;
+        case pool:
+            activatePoolLayer((PoolLayer *) layer, in);
+            break;
+    }
+}
+
+void backPropLayer(Layer* layer, Tensor* nextLayerBack){
+    switch(layer->type){
+        case conv:
+            backPropConvLayer((ConvLayer *) layer, nextLayerBack);
+            break;
+        case fc:
+            backPropFcLayer((FcLayer *) layer, nextLayerBack);
+            break;
+        case relu:
+            backPropReluLayer((ReluLayer *) layer, nextLayerBack);
+            break;
+        case pool:
+            backPropPoolLayer((PoolLayer *) layer, nextLayerBack);
+            break;
+    }
+}
+
