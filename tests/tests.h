9d60d16903a050082837419baa1bbc7a6a5f93ff
EB <woda33@vp.pl>
2017-12-02 23:26:17 +0100

    Finished tests for math library and corected tensor.c file

--- tests/tests.h
+++ tests/tests.h
@@ -10,5 +10,19 @@ void TEST_POINT_ALL();
 
 //VECTOR
 void TEST_initVector();
+void TEST_freeVector();
+void TEST_pushBackVector();
+void TEST_getVectorField();
+void TEST_VECTOR_ALL();
+
+//TENSOR
+void TEST_initTensor();
+void TEST_freeTensor();
+void TEST_copyTensor();
+void TEST_addTensor();
+void TEST_subTensor();
+void TEST_getTensorField();
+void TEST_getFasterTensorField();
+void TEST_TENSOR_ALL();
 
 #endif //SCOTR_TESTS_H
