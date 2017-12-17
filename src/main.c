d6e1f872c37de1d65ceec1504cd6050b715eeb88
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-12-17 23:20:04 +0100

    uncomment

--- src/main.c
+++ src/main.c
@@ -9,12 +9,12 @@ int main() {
     srand(time(NULL));
     Point point = {200,100,3};
     Cnn* cnn = initCnn(&point);
-//    addConvLayer(cnn,1,5,10,2);
-//    addReluLayer(cnn);
-//    addPoolLayer(cnn,2,2);
-//    addConvLayer(cnn,1,3,10,1);
-//    addReluLayer(cnn);
-//    addPoolLayer(cnn,2,2);
+    addConvLayer(cnn,1,5,10,2);
+    addReluLayer(cnn);
+    addPoolLayer(cnn,2,2);
+    addConvLayer(cnn,1,3,10,1);
+    addReluLayer(cnn);
+    addPoolLayer(cnn,2,2);
     addFcLayer(cnn,80);
     addReluLayer(cnn);
     addFcLayer(cnn,10);
